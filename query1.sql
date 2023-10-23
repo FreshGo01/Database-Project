6514643644ad17de070d4660ca65d69fc5459a5a
FreshGo01 <freshnathawat@gmail.com>
2023-10-23 21:14:58 +0700

    add testQuery.sql

--- query1.sql
+++ query1.sql
@@ -1,8 +1,18 @@
 .mode table
 -- who study in master degree and miss class more than 2 times in all subjects and who is professor advisor
-SELECT S.Student_ID, S.Student_Name, P.Professor_Name 
+.print 'who study in master degree and miss class more than 2 times in all subjects and who is professor advisor'
+SELECT S.Student_ID AS 'Student ID' , S.Student_Name AS 'Student Name', P.Professor_Name AS 'Advisor' , MissClass
 FROM Student AS S
 INNER JOIN Professor AS P ON S.Professor_ID = P.Professor_ID
+INNER JOIN (
+    SELECT SA.Student_ID, COUNT(*) AS MissClass
+    FROM StudentAttendance AS SA
+    INNER JOIN ClassAttendance AS CA ON SA.ClassAttendance_ID = CA.ClassAttendance_ID
+    INNER JOIN ClassRoomSchedule AS CRS ON CA.ClassRoomSchedule_ID = CRS.ClassRoomSchedule_ID
+    WHERE SA.Student_Check = 0
+    GROUP BY SA.Student_ID
+    HAVING COUNT(*) > 2
+) AS CountMiss ON S.Student_ID = CountMiss.Student_ID
 WHERE S.Student_Degree = 'Master'
 AND S.Student_ID IN (
     SELECT SA.Student_ID
@@ -13,3 +23,5 @@ AND S.Student_ID IN (
     GROUP BY SA.Student_ID
     HAVING COUNT(*) > 2
 );
+
+
---
+++ testQuery.sql
@@ -0,0 +1,4 @@
+SELECT
+    TIME(ClassAttendance_Professor_Time_In) AS Professor_Check_In_Time
+FROM
+    ClassAttendance;
